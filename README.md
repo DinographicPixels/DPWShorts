45cf90060351bd5bd9e3ff576f18adc87d0219a3
pakkographic <sinpli7@gmail.com>
2021-06-03 00:29:27 +0200

    v.1.0.5
    Fixed console function
    New readme doc

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # DPWShorts
- Execute basic tasks more faster.
- This package has been made especially for us, a lot of things are useless for your usages.
- This version isn't stable (Pre-Alpha version), it may don't work.
+### Pre-Alpha Documentation
+**Features :**
+* Error/Warning/Info Console
+* *New features coming soon..*
+
+Get started
+-
+---
+```
+const DPWShorts = require("dpwshorts")
+
+const DPWS = new DPWShorts()
+```
+Err,Warn,Info Console usage
+-
+```
+DPWS.Console(Type, Desc) 
+//Example : DPWS.Console("ERR", "Couldn't execute test()")
+// For Type you can use ERR, WARN or INFO
+```
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-function DPWConsole(Type, Desc) {
-    if (Type == "ERR" || Type == "WARN" || Type == "INFO"){
-        console.log(`DPW ${Type}: ${Desc}`)
-    }else{
-        console.log("DPW: Couldn't load DPWConsole()")
-        console.log("DPW: Invalid Type, try again with ERR or WARN or INFO")
-    }
-}
+module.exports = class DPWS {
 
-module.exports = DPWConsole
+    Console(Type, Desc) {
+        if (Type == "ERR" || Type == "WARN" || Type == "INFO"){
+            console.log(`DPW ${Type}: ${Desc}`)
+        }else{
+            console.log("DPW: Couldn't load DPWConsole()")
+            console.log("DPW: Invalid Type, try again with ERR or WARN or INFO")
+        }
+    }
+    
+    }
+    
