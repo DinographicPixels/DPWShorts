8d10e45cf50dc452f3e8e3c2e13b9b1aea2f34a8
pakkographic <sinpli7@gmail.com>
2021-06-03 01:22:28 +0200

    v.1.0.7-v.1.0.8
    Added colors to Console()
    Added the NoDescWarn property
    Added Invalid Type error
    Added No desc set warning (can be disabled by setting the NoDescWarn property to false)
    Modified the readme doc
    and more..

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # DPWShorts
 ### Pre-Alpha Documentation
 **Features :**
-* Error/Warning/Info Console
+* Error/Warning/Info Console Logging (with colors!)
 * *New features coming soon..*
 
 Get started
@@ -15,7 +15,11 @@ const DPWS = new DPWShorts()
 Err,Warn,Info Console usage
 -
 ```
-DPWS.Console(Type, Desc) 
-//Example : DPWS.Console("ERR", "Couldn't execute test()")
+DPWS.Console(Type, Desc, NoDescWarn) 
+// Example : DPWS.Console("ERR", "Couldn't execute test()")
 // For Type you can use ERR, WARN or INFO
+
+// There's a warning if you don't set the Desc property but you can ignore it by turning on the NoDescWarn property.
+// Example : DPWS.Console("ERR", "", true) <- it will ignore the warning
+// Example : DPWS.Console("ERR", "", true) <- it will show the warning
 ```
--- main.js
+++ main.js
@@ -1,11 +1,21 @@
+var colors = require('colors')
+
 module.exports = class DPWS {
 
-    Console(Type, Desc) {
+    Console(Type, Desc, NoDescWarn) {
+        if(NoDescWarn == false || NoDescWarn == undefined || NoDescWarn == ""){
+            if (Desc == undefined || Desc == "" || Desc == null){ console.log(`DPWShorts WARN: There's no description set.`.underline.bgYellow.black) }}
         if (Type == "ERR" || Type == "WARN" || Type == "INFO"){
-            console.log(`DPW ${Type}: ${Desc}`)
+            if (Type == "ERR"){
+            console.log(`DPW ${Type}:`.underline.red, `${Desc}`.red)
+            }else if(Type == "WARN"){
+            console.log(`DPW ${Type}:`.underline.yellow, `${Desc}`.yellow)
+            }else if(Type == "INFO"){
+            console.log(`DPW ${Type}:`.underline.grey, `${Desc}`.grey)
+            }
         }else{
-            console.log("DPW: Couldn't load DPWConsole()")
-            console.log("DPW: Invalid Type, try again with ERR or WARN or INFO")
+            console.log("DPWShorts: Couldn't load DPWConsole()".underline.bgRed.white)
+            console.log("DPWShorts: Invalid Type, try again with ERR or WARN or INFO".underline.bgRed.white)
         }
     }
     
