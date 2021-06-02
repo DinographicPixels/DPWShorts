1dec3642c0374c272f8c39bd879df506c9a77a78
pakkographic <sinpli7@gmail.com>
2021-06-02 23:27:38 +0200

    v.1.0.4 (pre-alpha)
    replaced WarningType with Type

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function DPWConsole(Type, Desc) {
-    if (WarningType == "ERR" || WarningType == "WARN" || WarningType == "INFO"){
-        console.log(`DPW ${WarningType}: ${Desc}`)
+    if (Type == "ERR" || Type == "WARN" || Type == "INFO"){
+        console.log(`DPW ${Type}: ${Desc}`)
     }else{
         console.log("DPW: Couldn't load DPWConsole()")
-        console.log("DPW: Invalid WarningType, try again with ERR or WARN or INFO")
+        console.log("DPW: Invalid Type, try again with ERR or WARN or INFO")
     }
 }
 
