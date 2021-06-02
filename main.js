7653a339c92626e41ad90786200c61c98eab19ec
pakkographic <sinpli7@gmail.com>
2021-06-03 01:54:44 +0200

    v.1.0.9
    Fixed NoDescWarn property bug.
    New NoDescWarn invalid value error.
    and more..

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ var colors = require('colors')
 module.exports = class DPWS {
 
     Console(Type, Desc, NoDescWarn) {
-        if(NoDescWarn == false || NoDescWarn == undefined || NoDescWarn == ""){
+        if(NoDescWarn !== true){
             if (Desc == undefined || Desc == "" || Desc == null){ console.log(`DPWShorts WARN: There's no description set.`.underline.bgYellow.black) }}
+        
+        if (NoDescWarn !== true && NoDescWarn !== false){
+            if (NoDescWarn !== undefined){
+                console.log(`DPWShorts ERROR: NoDescWarn has an invalid value: ${NoDescWarn}.`.underline.bgRed.white)
+                console.log(`DPWShorts INFO: NoDescWarn working values : true, false`.underline.grey)
+            }
+        }
         if (Type == "ERR" || Type == "WARN" || Type == "INFO"){
             if (Type == "ERR"){
             console.log(`DPW ${Type}:`.underline.red, `${Desc}`.red)
@@ -14,8 +21,8 @@ module.exports = class DPWS {
             console.log(`DPW ${Type}:`.underline.grey, `${Desc}`.grey)
             }
         }else{
-            console.log("DPWShorts: Couldn't load DPWConsole()".underline.bgRed.white)
-            console.log("DPWShorts: Invalid Type, try again with ERR or WARN or INFO".underline.bgRed.white)
+            console.log("DPWShorts ERROR: Couldn't load DPWConsole()".underline.bgRed.white)
+            console.log("DPWShorts ERROR: Invalid Type, try again with ERR or WARN or INFO".underline.bgRed.white)
         }
     }
     
